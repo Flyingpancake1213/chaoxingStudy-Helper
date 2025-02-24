3a56649592d89bd2f7505dc8789ebfaf4244ed54
xuebi <114483499+myxuebi@users.noreply.github.com>
2025-02-24 12:45:59 +0800

    更新界面

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,29 @@
   <div class="common-layout">
     <el-container>
       <el-card style="width: 100%; height: 95%">
-        <template #header><div  style="text-align: center;font-weight: bold">学习通AI自动考试程序</div></template>
-        <div>
-          <div>学习通账号：<el-input v-model="username" style="width: 40%;margin-left: 10%" placeholder="请输入账号" /></div>
-          <div style="margin-top: 10px">学习通密码：<el-input type="password" v-model="password" style="width: 40%;margin-left: 10%" placeholder="请输入密码" show-password /></div>
-          <div style="margin-top: 10px">考试的链接：<el-input v-model="cxurl" style="width: 40%;margin-left: 10%" placeholder="进入考试后的第一题链接" /></div>
-          <div style="margin-top: 10px">通义Token： <el-input v-model="tyapi" style="width: 40%;margin-left: 10%" placeholder="通义千问API TOKEN" /></div>
+        <template #header>
+          <div  style="text-align: center;font-weight: bold">学习通AI自动考试程序</div></template>
+        <div style="display: flex; align-content:flex-start">
+          <div style="flex:3;">
+            <div>学习通账号：<el-input v-model="username" style="width: 50%;margin-left: 10%" placeholder="请输入账号" /></div>
+            <div style="margin-top: 10px">学习通密码：<el-input type="password" v-model="password" style="width: 50%;margin-left: 10%" placeholder="请输入密码" show-password /></div>
+            <div style="margin-top: 10px">考试的链接：<el-input v-model="cxurl" style="width: 50%;margin-left: 10%" placeholder="进入考试后的第一题链接" /></div>
+            <div style="margin-top: 10px">通义Token： <el-input v-model="tyapi" style="width: 50%;margin-left: 10%" placeholder="通义千问API TOKEN" /><a @click="startie('https://bailian.console.aliyun.com/?apiKey=1#/api-key')" style="margin-left: 10px" href="">申请API</a></div>
+          </div>
+          <div style="flex: 1; color: #666; font-size: 12px;">
+            <div style="font-weight: bold; margin-bottom: 10px;">免责声明：</div>
+            <div style="line-height: 1.5;">
+              <p>本程序仅供学习和测试使用，请在下载24小时内删除，切勿用于考试舞弊等行为，所有使用本程序导致的后果皆由使用者自行承担，本项目不得用于违法违规用途，使用本程序即代表您同意此免责声明</p>
+            </div>
+          </div>
         </div>
         <template #footer>
           <div style="text-align: center">
             <el-button type="primary" @click="runCmd">开始考试</el-button>
             <div style="text-align: left">日志输出：</div>
-            <pre id="output" style="text-align: left; margin-top: 10px; background: #f5f5f5; padding: 10px; border-radius: 4px; max-height: 200px; overflow-y: auto;"></pre>          </div>
+            <pre id="output" style="text-align: left; margin-top: 10px; background: #f5f5f5; padding: 10px; border-radius: 4px; max-height: calc(100vh - 430px); overflow-y: auto;"></pre>
+            <div><a href="" @click="startie('https://github.com/myxuebi/chaoxingStudy-exam')">Github:myxuebi/chaoxingStudy-exam</a></div>
+          </div>
         </template>
       </el-card>
     </el-container>
@@ -41,4 +52,14 @@ onMounted(() => {
     output.scrollTop = output.scrollHeight;
   });
 });
+
+function startie(url){
+  window.electronAPI.send('start-ie',url)
+}
 </script>
+
+<style>
+a, a:visited, a:link, a:hover{
+  color:blue;
+}
+</style>
